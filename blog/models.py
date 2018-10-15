d8e14090e749662e099cc717cd8fdfcdfde941dc
Artem <artem2000200@mail.ru>
2018-10-15 18:02:02 +0300

    Version_with_Passwords

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    text = models.CharField(max_length=200)
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
@@ -13,6 +13,8 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+
+
 
     def __str__(self):
         return self.title
